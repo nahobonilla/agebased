69e249ca9f74050b167808312e63cd8696c5b4b2
Delmy Bonilla <bonildel001@warren.k12.in.us>
2024-10-28 07:31:04 -0400

    Initial Commit1

---
+++ main.py
@@ -0,0 +1,9 @@
+age = int(input("Please enter your age:"))
+if age <5:
+    print("You should play with toys")
+elif 5 <= age <= 12:
+    print("You can play outdoor game.")
+elif 13 <= age <= 17:
+    print("You might enjoy video games")
+elif age >= 18:
+    print("You can try hiking or traveling")
